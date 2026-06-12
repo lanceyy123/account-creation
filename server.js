0622bdf96afd577f4a64e965fa45dae484f42198
Lance <lncaviso@gmail.com>
2026-06-12 18:15:39 +0800

    fix railway proxy

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import rateLimit from "express-rate-limit";
 
 const otpAttempts = new Map();
 const app = express();
+app.set("trust proxy", 1);
 const otpLimiter = rateLimit({
     windowMs: 3 * 60 * 1000,
     max: 5,
